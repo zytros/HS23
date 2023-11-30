400c2059df2f5b5dcb2609fc45f54d41961b550b
Luca Sichi <luca.sichi@hotmail.com>
2023-11-30 10:19:40 +0100

    isl

---
+++ InfSec_Lab/Part4/solve_template.py
@@ -0,0 +1,47 @@
+import json
+import logging
+import sys
+import os
+import socket
+
+# Change the port to match the challenge you're solving
+PORT = ...
+
+# Pro tip: for debugging, set the level to logging.DEBUG if you want
+# to read all the messages back and forth from the server
+# log_level = logging.DEBUG
+log_level = logging.INFO
+logging.basicConfig(stream=sys.stdout, level=log_level)
+
+s = socket.socket()
+
+# Set the environmental variable REMOTE to True in order to connect to the server
+#
+# To do so, run on the terminal:
+# REMOTE=True sage solve.py
+#
+# When we grade, we will automatically set this for you
+if "REMOTE" in os.environ:
+    s.connect(("isl.aclabs.ethz.ch", PORT))
+else:
+    s.connect(("localhost", PORT))
+
+fd = s.makefile("rw")
+
+
+def json_recv():
+    """Receive a serialized json object from the server and deserialize it"""
+
+    line = fd.readline()
+    logging.debug(f"Recv: {line}")
+    return json.loads(line)
+
+def json_send(obj):
+    """Convert the object to json and send to the server"""
+
+    request = json.dumps(obj)
+    logging.debug(f"Send: {request}")
+    fd.write(request + "\n")
+    fd.flush()
+
+# WRITE YOUR SOLUTION HERE
